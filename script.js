75923b01e8441577cadd484d99e4356d42f87dd6
Shane Tierney <sptierney713@gmail.com>
2022-03-27 17:20:38 -0400

    adds dropdown box to select operator to fulfil homework

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 function calculate() {
     var numOne = document.getElementById('firstNum').value
     var numTwo = document.getElementById('secondNum').value
-    var answer = Number(numOne) + Number(numTwo)
+    var oper = document.getElementById('operator').value
+    var answerElement = document.getElementById('answer')
 
-    document.getElementById('answer').innerHTML = answer
+    console.log(oper)
+    if (oper === 'addition'){
+        var addAns = Number(numOne) + Number(numTwo)
+        answerElement.innerHTML = addAns
+    } else if (oper === 'subtraction') {
+        var subAns = Number(numOne) - Number(numTwo)
+        answerElement.innerHTML = subAns
+    } else if (oper === 'multiplication') {
+        var multAns = Number(numOne) * Number(numTwo)
+        answerElement.innerHTML = multAns
+    } else if (oper === 'division') {
+        var divAns = Number(numOne) / Number(numTwo)
+        answerElement.innerHTML = divAns
+    }
+    
+    
 }
 
 
